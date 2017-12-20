530af3f3353e2a0aa8a5946b2c604e6019177e32
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2017-12-20 19:52:12 +0100

    initial commit
    the dot propagator is somewhat working, but will probably show weak
    points in some cases.

---
+++ Makefile
@@ -0,0 +1,20 @@
+
+tree:
+	$(MAKE) dot TEST_CASE=tree
+hole:
+	$(MAKE) dot TEST_CASE=pigeon-hole
+euler:
+	$(MAKE) dot TEST_CASE=euler
+fizzbuzz:
+	$(MAKE) dot TEST_CASE=fizzbuzz
+truth1:
+	$(MAKE) dot TEST_CASE=truth1
+
+
+dot: dot_run dot_conv dot_show
+dot_run:
+	- clingo -n 0 dotpropagator.lp ./test_cases/$(TEST_CASE).lp
+dot_conv:
+	dot -Tpng out.dot > out.png
+dot_show: dot_conv
+	feh out.png
---
+++ dotpropagator.py
@@ -0,0 +1,73 @@
+
+from collections import defaultdict
+
+class DotPropagator:
+    """A non-literal *dot* propagator"""
+
+    def __init__(self, dotfiledesc:str):
+        self.__dotfiledesc = dotfiledesc
+        self.__stack = ['Start']
+        self.__step = 0
+        self.__stable_nodes = set()  # all checked nodes in the graph
+        self.write_header()
+
+    def init(self, init):
+        self.__literals = {atom.symbol: init.solver_literal(atom.literal) for atom in init.symbolic_atoms}
+        self.__symbols = defaultdict(set)
+        # print('INIT:', self.__literals)
+        for atom in init.symbolic_atoms:
+            lit = init.solver_literal(atom.literal)
+            self.__symbols[lit].add(atom)
+            init.add_watch(lit)
+
+    def propagate(self, ctl, changes):
+        level = ctl.assignment.decision_level
+        # print('PROPAGATE:', changes)
+        self.__step += 1
+        for lit in changes:
+            self.write('\t{} -> {} [label="{}"];'.format(self.last_node, lit, self.__step))
+            self.push_node(lit)
+
+    def undo(self, solver_id, assign, undo):
+        # print('UNDO:', undo)
+        prev_node = self.pull_node()
+        self.write('\t{} -> {} [style=dotted label={}];'.format(prev_node, self.last_node, self.__step))
+
+    def check(self, ctl):
+        self.__stable_nodes.add(self.last_node)
+
+
+    def pull_node(self):
+        *self.__stack, ret = self.__stack
+        return ret
+    def push_node(self, uid):
+        self.__stack.append(uid)
+
+    @property
+    def last_node(self):
+        return self.__stack[-1]
+
+    def write(self, line):
+        self.__dotfiledesc.write(line.rstrip() + '\n')
+
+    def write_nodes(self):
+        for uid, atoms in self.__symbols.items():
+            self.write('\t{} [label="{}"{}];'.format(
+                uid, ' '.join(str(atom.symbol) for atom in atoms),
+                ' fillcolor="green"' if uid in self.__stable_nodes else ''
+            ))
+    def write_header(self):
+        self.write('digraph solving {')
+        self.write('\tnode [style=filled];')
+    def finalize(self):
+        self.write_nodes()
+        self.write('}')
+
+
+def main(prg):
+    with open('out.dot', 'w') as fd:
+        propagator = DotPropagator(fd)
+        prg.register_propagator(propagator)
+        prg.ground([("base", [])])
+        prg.solve()
+        propagator.finalize()
